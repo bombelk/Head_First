2c73b0723f21bf46b1eef1e630bbee401e88bb41
kacper <kacper.babka@gmail.com>
2023-05-05 11:04:11 +0200

    Big commit

---
+++ app/src/main/java/com/example/trenaer/DetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.trenaer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        WorkoutDetailFragment frag = (WorkoutDetailFragment)
+                getSupportFragmentManager().findFragmentById(R.id.detail_frag);
+        frag.setWorkoutId(1);
+    }
+}
